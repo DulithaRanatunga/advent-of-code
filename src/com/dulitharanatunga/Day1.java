3900363e76c59c0eca4466015c7c3f0ecd98651e
Dulitha Ranatunga <dranatunga@squiz.net>
2021-12-02 17:57:27 +1100

    day1_part2

--- src/com/dulitharanatunga/Day1.java
+++ src/com/dulitharanatunga/Day1.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day1 {
 
@@ -15,8 +16,7 @@ public class Day1 {
        return calculateIncreases("day1");
     }
 
-    private int calculateIncreases(String fileName) {
-        List<String> lines = readInput(fileName);
+    private int part1(List<String> lines) {
         // Challenge: calculate number of lines that are more than the previous.
         Integer prevDepth = null;
         int increases = 0;
@@ -28,6 +28,41 @@ public class Day1 {
             prevDepth = current;
         }
         return increases;
+    }
+
+    private int part2(List<String> lines) {
+        // Challenge: calculate number of lines that are more than the previous.
+        // But use a sliding window of size 3.
+        List<Integer> parsed = lines.stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+        if (lines.size() < 3) {
+            // Data too small. Abort!
+            return 0;
+        }
+
+        // Could replace with an array pop/push, but hardcoding because lazy for this size.
+        Integer window0 = parsed.get(0);
+        Integer window1 = parsed.get(1);
+        Integer window2 = parsed.get(2);
+        Integer prevWindow = window0 + window1 + window2;
+        int increases = 0;
+        for (int i =3; i < parsed.size(); i++) {
+            Integer current = parsed.get(i);
+            int currentWindow = window1 + window2 + current;
+            if (prevWindow != null && prevWindow < currentWindow) {
+                increases++;
+            }
+            prevWindow = currentWindow;
+            window0 = window1;
+            window1 = window2;
+            window2 = current;
+        }
+        return increases;
+    }
+
+    private int calculateIncreases(String fileName) {
+        List<String> lines = readInput(fileName);
+        // return part1(lines);
+        return part2(lines);
     }
 
     // Reads the input file and returns a list of lines.
