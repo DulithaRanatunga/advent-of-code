9f5e7360a3f0f3a6a37757d600cf8d34810b1f61
Dulitha Ranatunga <dranatunga@squiz.net>
2022-12-03 12:37:18 +1100

    add day 2

--- src/main/java/com/dulitharanatunga/Main.java
+++ src/main/java/com/dulitharanatunga/Main.java
@@ -5,7 +5,7 @@ import com.dulitharanatunga._2022.*;
 public class Main {
 
     public static void main(String[] args) {
-        Day day = new Day1();
+        Day day = new Day2();
 	    System.out.println("Sample:" + day.runSample());
 	    System.out.println("Full:" + day.run());
     }
